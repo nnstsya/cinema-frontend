c965a13d7b9c3901dcb39252f3851d6bafbe1bd1
nnstsya <tonastyaket@gmail.com>
2025-06-03 19:21:06 +0300

    bug fixes

--- src/components/others/posterPage.vue
+++ src/components/others/posterPage.vue
@@ -29,17 +29,14 @@
             </div>
 
             <div class="hover-actions">
-              <button class="play-button">
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5 3L19 12L5 21V3Z" fill="currentColor"/>
-                </svg>
-                Play Now
-              </button>
-              <button class="add-button" :title="`Add ${movie.title} to watchlist`">
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12 5V19M5 12H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-              </button>
+              <a :href="movie.trailerLink" target="_blank" rel="noopener noreferrer">
+                <button class="play-button">
+                  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5 3L19 12L5 21V3Z" fill="currentColor"/>
+                  </svg>
+                  Play Now
+                </button>
+              </a>
             </div>
           </div>
         </div>
